4354b5b38e083ece66663561aa737296a26ef426
mkfizi <mkfizi93@gmail.com>
2022-06-24 03:42:18 +0800

    Add dark mode module

--- src/js/app.js
+++ src/js/app.js
@@ -1,43 +1,17 @@
-/**
- * Add and remove 'transition-none' classes to elements that have any 
- * 'transition' and 'transition-*' classes to avoid any animations effect
- * when toggling dark mode.
- */
-const toggleTransitions = () => {
-    let elements = document.querySelectorAll(`.transition, 
-                                            .transition-all, 
-                                            .transition-colors, 
-                                            .transition-opacity, 
-                                            .transition-shadow, 
-                                            .transition-transform`);
+import darkMode from "./components/dark-mode.js";
 
-    for (let i = 0; i < elements.length; i++) {
-        let element = elements[i];
-        element.classList.add("transition-none");
-        setTimeout(() => { element.classList.remove("transition-none"); }, 1000);
-    }
-}
+"use strict";
 
-/**
- *  Toggles dark mode. 
- */
-const toggleDarkMode = () => {
-    toggleTransitions();
+export let app = {
+    init: () => {
+        let components = {
+            darkMode,
+        };
 
-    if (localStorage.theme === "light" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: light)").matches)) {
-        localStorage.theme = "dark";
-        document.documentElement.classList.add("dark");
-    } else if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
-        localStorage.theme = "light";
-        document.documentElement.classList.remove("dark")
-    }; 
-}
+        Object.values(components).forEach(component => {
+            if (component.initialize != null) component.initialize();
+        });
+    },
+};
 
-/**
- *  Add event listener for dark mode toggle to button and window. This code
- *  executes when window is loaded. 
- */
-window.onload = () => {
-    document.getElementById("darkModeToggle").addEventListener("click", () => toggleDarkMode());
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => toggleDarkMode());
-}
+app.init();
---
+++ src/js/components/dark-mode.js
@@ -0,0 +1,33 @@
+class DarkMode {
+    constructor(element) {
+        element.addEventListener("click", () => this.toggle());
+    }
+
+    toggle() {
+        let transitions = document.querySelectorAll(".transition, .transition-all, .transition-colors, .transition-opacity, .transition-shadow, .transition-transform");
+        for (let i = 0; i < transitions.length; i++) {
+            transitions[i].classList.add("transition-none");
+            setTimeout(() => { transitions[i].classList.remove("transition-none"); }, 1000);
+        }
+
+        if (localStorage.theme === "light" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: light)").matches)) {
+            localStorage.theme = "dark";
+            document.documentElement.classList.add("dark");
+        } else if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+            localStorage.theme = "light";
+            document.documentElement.classList.remove("dark")
+        }; 
+    }
+}
+
+const darkMode = {
+    initialize() {
+        let components = document.querySelectorAll("[data-component='darkMode']");
+
+        for (let i = 0; i < components.length; i++){
+            new DarkMode(components[i]);
+        }
+    }
+}
+
+export default darkMode;
