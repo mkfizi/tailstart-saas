b4286b76ee0b0e5d29c070ee8b54c49061e4c9e0
mkfizi <mkfizi93@gmail.com>
2022-06-24 23:32:16 +0800

    @ +navbar

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import DarkMode from "./components/dark-mode.js";
+import Navbar from "./components/navbar.js";
 
 "use strict";
 
@@ -6,6 +7,7 @@ export let app = {
     init: () => {
         let initialize = [
             DarkMode,
+            Navbar,
         ];
 
         for (let i = 0; i < initialize.length; i++) {
