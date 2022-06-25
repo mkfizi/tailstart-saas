95b7df3582e123a03ecd14be97c5b2b48b8bc07e
mkfizi <mkfizi93@gmail.com>
2022-06-26 01:37:56 +0800

    @ +sidebar

---
+++ src/js/component/sidebar.js
@@ -0,0 +1,42 @@
+import {initializeObjectByQuery} from "../config.js";
+
+class Sidebar {
+    constructor(element) {
+        this.container = element;
+
+        if (element.getAttribute("id") == null) return;
+
+        this.id = element.getAttribute("id");
+
+        let buttons = document.querySelectorAll("[data-target='"+this.id+"']");
+        for (let i = 0; i < buttons.length; i++){
+            if (buttons[i].dataset.event == null) return;
+            if (buttons[i].dataset.event == "show") this.buttonShow = buttons[i];
+            if (buttons[i].dataset.event == "hide") this.buttonHide = buttons[i];
+        }
+
+        if (this.buttonShow != null) this.buttonShow.addEventListener("click", () => this.show())
+        if (this.buttonHide != null) this.buttonHide.addEventListener("click", () => this.hide())
+    }
+
+    show() {
+        if (this.container.classList.contains("-translate-x-full")) this.position = "-translate-x-full";
+        if (this.container.classList.contains("translate-x-full")) this.position = "translate-x-full";
+
+        this.container.classList.remove(this.position);
+        // FocusTrap(this.container);
+        // Backdrop(this.container);
+    }
+
+    hide() {
+        this.container.classList.add(this.position);
+        // FocusTrap();
+    }
+}
+
+const QUERY = "[data-component='Sidebar']";
+const OBJECT = Sidebar;
+
+initializeObjectByQuery(QUERY, OBJECT);
+
+export default Sidebar;
