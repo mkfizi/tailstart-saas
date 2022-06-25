1a0bcaccb659cb990eb6a32a795db71abb224241
mkfizi <mkfizi93@gmail.com>
2022-06-26 01:34:39 +0800

    @ +config methods

---
+++ src/js/config.js
@@ -0,0 +1,15 @@
+const initializeObjectById = (id, object) => {
+    let element = document.getElementById(id);
+    if (element != null) new object(element);
+}
+const initializeObjectByQuery = (query, object) => {
+    let element = document.querySelectorAll(query);
+    for (let i = 0; i < element.length; i++) {
+        new object(element[i]);
+    }
+}
+
+export {
+    initializeObjectById,
+    initializeObjectByQuery
+}
