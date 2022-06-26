1008869f0feb28f855fcfc84a1c2be14e6ab8960
mkfizi <mkfizi93@gmail.com>
2022-06-26 21:58:03 +0800

    Add use strict

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
+"use strict";
+
 import DarkMode from "./component/dark-mode.js"
 import Dashboard from "./component/dashboard.js"
 import Sidebar from "./component/sidebar.js"
-
-// import Backdrop from "./util/backdrop.js"
