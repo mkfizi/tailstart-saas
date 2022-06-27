e2de72eb199029c37526a7d0011f8820f7082cb9
mkfizi <mkfizi93@gmail.com>
2022-06-27 11:09:19 +0800

    % ^pass object to focus trap

--- src/js/util/focus-trap.js
+++ src/js/util/focus-trap.js
@@ -1,12 +1,12 @@
 class FocusTrap{
-    constructor(container) {
-        this.container = container;
+    constructor(object) {
+        this.activeObject = object;
 
-        this.container.setAttribute("tabindex", 0);
-        setTimeout(() => { this.container.removeAttribute("tabindex"); }, 1000);
-        this.container.focus();
+        this.activeObject.container.setAttribute("tabindex", 0);
+        setTimeout(() => { this.activeObject.container.removeAttribute("tabindex"); }, 1000);
+        this.activeObject.container.focus();
 
-        let focusableElements = this.container.querySelectorAll("a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, [tabindex=\"0\"], [contenteditable]"); 
+        let focusableElements = this.activeObject.container.querySelectorAll("a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, [tabindex=\"0\"], [contenteditable]"); 
         this.firstElement = focusableElements[0];
         this.lastElement = focusableElements[focusableElements.length - 1];
 
