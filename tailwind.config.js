4e4a601e44016e0a73f67b4147f8d92c93b3895c
mkfizi <mkfizi93@gmail.com>
2022-06-30 23:38:48 +0800

    @ +viewport fix for mobile browsers

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     mode: 'jit',
     darkMode: 'class',
     content: [
-		"./*.html",
-		"./src/**/*.{html,js}"
+        "./*.html",
+        "./src/**/*.{html,js}"
     ],
-    theme: {
-      	extend: {},
+    theme: {	
+        extend: {
+            minHeight: {
+                "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
+            },
+            height: {
+                "screen": ["100vh", "calc(var(--vh, 1vh) * 100)"],
+            }
+        },
     },
-    plugins: [
-      	require('@tailwindcss/forms'),
-    ],
+    plugins: [],
 }
