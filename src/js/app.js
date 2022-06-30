3bbb1b98280de2c1604e21b0f0a334b0984dea2e
mkfizi <mkfizi93@gmail.com>
2022-06-30 23:38:48 +0800

    Add viewport height fix for mobile browsers

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,21 @@ import Collapse from "./component/collapse.js"
 import DarkMode from "./component/dark-mode.js"
 import Dashboard from "./component/dashboard.js"
 import Sidebar from "./component/sidebar.js"
+
+import {setFooterCurrentYear} from "./util/config.js"
+import {handleViewport} from "./util/config.js"
+
+const app = {
+    initialize: () => {
+        window.onload = () => {
+            handleViewport();
+            setFooterCurrentYear();
+        }
+
+        window.onresize = () => {
+            handleViewport();
+        }
+    }
+}
+
+app.initialize();
