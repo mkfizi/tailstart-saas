3beea39ad002e1f29bfc094050031b454a48dc0f
mkfizi <mkfizi93@gmail.com>
2022-07-02 01:41:15 +0800

    Change sidebar `element` to `container`

--- src/js/component/sidebar.js
+++ src/js/component/sidebar.js
@@ -4,46 +4,72 @@ import Backdrop from "../util/backdrop.js";
 import FocusTrap from "../util/focus-trap.js";
 
 class Sidebar{
+    container = null;   // Sidebar element
+    position = null;    // Sidebar position
+    buttons = []
+
+    /**
+     * Sidebar constructor.
+     * @params (HTMLDom) element
+     */
     constructor(container) {
         this.container = container;
-        if (this.container.classList.contains("-translate-x-full")) this.position = "-translate-x-full";
-        if (this.container.classList.contains("translate-x-full")) this.position = "translate-x-full";
-        this.initialize();
+        
+        this.setPosition();
+        this.setButton();
     }
 
-    initialize() {
+    /**
+     * Handle event.
+     * @params (object) event
+     */
+    handleEvent(event) {
+        if (event.currentTarget.dataset.action == "show" && event.type == "click") this.show();
+        if (event.currentTarget.dataset.action == "hide" && event.type == "click") this.hide();
+    }
+    
+    /**
+     * Show sidebar.
+     */
+    show() {
+        this.container.classList.remove(this.position);
+        this.focusTrap = new FocusTrap(this);
+        this.backdrop = new Backdrop(this);
+    }
+
+    /**
+     * Hide sidebar.
+     */
+    hide() {
+        this.container.classList.add(this.position);
+        if (this.focusTrap != null && this.backdrop != null) {
+            this.focusTrap.destructor();
+            this.backdrop.destructor();
+            this.focusTrap = null;
+            this.backdrop = null;
+        }
+    }
+
+    /**
+     * Set sidebar position.
+     */
+    setPosition() {
+        if (this.container.classList.contains("-translate-x-full")) this.position = "-translate-x-full";
+        if (this.container.classList.contains("translate-x-full")) this.position = "translate-x-full";
+    }
+
+    /**
+     * Set sidebar buttons.
+     */
+    setButton() {
         if (this.container.getAttribute("id") == null) return null;
 
-        let id = this.container.getAttribute("id");
-        let buttons = document.querySelectorAll("[data-target='"+id+"']");
-        
+        let buttons = document.querySelectorAll(`[data-target="${this.container.getAttribute("id")}"]`);
         for (let i = 0; i < buttons.length; i++){
             buttons[i].addEventListener("click", this);
         }
     }
 
-    handleEvent(event) {
-        let action = event.currentTarget.dataset.action;
-        if (action == null) return null;
-        if (action == "show") this.show();
-        if (action == "hide") this.hide();
-    }
-
-    show() {
-        this.container.classList.remove(this.position);
-
-        this.focusTrap = new FocusTrap(this);
-        this.backdrop = new Backdrop(this);
-    }
-
-    hide() {
-        this.container.classList.add(this.position);
-        if (this.focusTrap == null && this.backdrop == null) return null;
-        this.focusTrap.destroy();
-        this.focusTrap = null;
-        this.backdrop.destroy();
-        this.backdrop = null;
-    }
 }
 
 const selector = "[data-component='Sidebar']";
