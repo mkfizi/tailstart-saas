5e1de0c5048bf160ee5ddf63bbcf48afc6b6339b
mkfizi <mkfizi93@gmail.com>
2022-07-01 12:15:21 +0800

    Remove initialize & add destructor methods in backdrop

--- src/js/util/backdrop.js
+++ src/js/util/backdrop.js
@@ -1,33 +1,49 @@
 class Backdrop {
+    container = null;       // Backdrop element
+    activeObject = null;    // Object which triggers backdrop 
+
+    /**
+     * Backdrop constructor
+     */
     constructor(object) {
         if (this.activeObject != null) this.hide();
         this.activeObject = object;
         
         let container = document.createElement("div");
-        container.setAttribute("class", "fixed w-screen h-screen bg-neutral-900 opacity-50 z-10 transition top-0 left-0");
 
         this.container = container;
-        this.initialize();
-    }
-
-    initialize() {
-        this.show();
+        this.container.setAttribute("class", "fixed w-screen h-screen bg-neutral-900 opacity-50 z-10 transition top-0 left-0");
         this.container.addEventListener("click", this)
+
+        this.show();
     }
 
-    destroy() {
+    /**
+     * Backdrop destructor
+     */
+    destructor() {
         this.activeObject = null;
         this.container.remove();
     }
-
-    handleEvent() {
-        this.hide();
+    
+    /**
+     * Handle event
+     * @params (object) event
+     */
+    handleEvent(event) {
+        if (event.type == "click") this.hide();
     }
 
+    /**
+     * Show backdrop.
+     */
     show() {
         this.activeObject.container.parentNode.insertBefore(this.container, this.activeObject.container);
     }
 
+    /**
+     * Hide backdrop.
+     */
     hide() {
         this.activeObject.hide();
     }
