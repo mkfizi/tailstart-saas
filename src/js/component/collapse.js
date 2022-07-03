55ef08af137e9761f730d69a413d1063e1fd7960
mkfizi <mkfizi93@gmail.com>
2022-07-04 00:23:49 +0800

    @ ^collapse class method define

--- src/js/component/collapse.js
+++ src/js/component/collapse.js
@@ -1,58 +1,87 @@
-import {initializeObjectByQuery} from "../util/config.js";
+import {setObjectByQuery} from "../util/config.js";
 
 class Collapse {
-    constructor(container) {
-        this.container = container;
-        this.initialize();
+    element = null;     // Collapse element
+
+    /**
+     * Sidebar constructor.
+     * @params (HTMLDom) element
+     */
+     constructor(element) {
+        this.setElement(element);
     }
 
-    initialize() {
-        // if (this.container.getAttribute("id") == null) return null;
-
-        // let id = this.container.getAttribute("id");
-        let buttons = this.container.querySelectorAll("[data-target]");
-        for (let i = 0; i < buttons.length; i++){
-            buttons[i].addEventListener("click", this);
+    /**
+     * Handle event.
+     * @params (object) event
+     */
+    handleEvent(event) {
+        if (event.type == "click") {
+            if (event.currentTarget.dataset.action == "toggle") this.toggle(event.currentTarget);
         }
     }
 
-    handleEvent(event) {
-        this.action = event.currentTarget.dataset.action;
-        this.targetElement = this.container.querySelector("[id='"+event.currentTarget.dataset.target+"']");
-        this.icon =  event.currentTarget.querySelector("[data-icon='Collapse']");
-        console.log(this);
-        if (this.action == null) return null;
-        if (this.action == "toggle") this.toggle();
+    /**
+     * Toggle collapse.
+     * @params (HTMLDom) currentTarget
+     */
+    toggle(currentTarget) {
+        let content = this.element.querySelector(`[id="${currentTarget.dataset.target}"]`);
+        let icon = this.element.querySelector(`[data-icon="${currentTarget.dataset.target}"]`);
+        content.classList.contains("h-0")
+            ? this.show(content, icon)
+            : this.hide(content, icon);
     }
 
-    toggle() {
-        this.targetElement.classList.contains("h-0")
-            ? this.show()
-            : this.hide();
+    /**
+     * Show collapse.
+     * @params (element) content
+     * @params (element) icon
+     */
+    show(content, icon) {
+        icon.classList.add("rotate-180");
+        content.classList.remove("h-0");
+
+        let containerHeight = content.clientHeight + "px";
+
+        content.style.height = "0";
+        setTimeout(() => content.style.height = containerHeight, 50);
+        setTimeout(() => content.removeAttribute("style"), 500);
     }
 
-    show() {
-        this.icon.classList.add("rotate-180");
-        this.targetElement.classList.remove("h-0")
-        let containerHeight = this.targetElement.clientHeight + "px";
-        this.targetElement.style.height = "0";
-        setTimeout(() => this.targetElement.style.height = containerHeight, 50);
-        setTimeout(() => this.targetElement.removeAttribute("style"), 500);
+    /**
+     * Hide collapse.
+     * @params (element) content
+     * @params (element) icon
+     */
+    hide(content, icon) {
+        icon.classList.remove("rotate-180");
+
+        let containerHeight = content.clientHeight + "px";
+
+        content.style.height = containerHeight;
+        setTimeout(() => content.style.height = "0", 50);
+        setTimeout(() => content.removeAttribute("style"), 500);
+        content.classList.add("h-0")
     }
 
-    hide() {
-        this.icon.classList.remove("rotate-180");
-        let containerHeight = this.targetElement.clientHeight + "px";
-        this.targetElement.style.height = containerHeight;
-        setTimeout(() => this.targetElement.style.height = "0", 50);
-        setTimeout(() => this.targetElement.removeAttribute("style"), 500);
-        this.targetElement.classList.add("h-0")
+
+    /**
+     * Set sidebar element.
+     * @params (HTMLDom) element
+     */
+     setElement(element) {
+        this.element = element;
+        let buttons = this.element.querySelectorAll("[data-target]");
+        for (let i = 0; i < buttons.length; i++){
+            buttons[i].addEventListener("click", this);
+        }
     }
 }
 
 const selector = "[data-component='Collapse']";
 const object = Collapse;
 
-initializeObjectByQuery(selector, object);
+setObjectByQuery(selector, object);
 
 export default Collapse;
